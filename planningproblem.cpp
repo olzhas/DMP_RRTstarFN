2b07986ea0a9923e3739ae5cd0655fb9c1ad9694
Olzhas Adiyatov <olzhas.adi@gmail.com>
2016-01-03 12:14:13 +0600

    Documented a memory leak

--- planningproblem.cpp
+++ planningproblem.cpp
@@ -110,9 +110,11 @@ void PlanningProblem::treeUpdate()
                 }
             }
             /*
+             *
+             * FIXME the following lines cause memory leak
             //if(!once){
                 for(size_t i=0; i<pdatNumVerticies; ++i){
-                    DrawableLiveTime* d = new DrawableLiveTime;
+                    DrawableLiveTime* d = new DrawableLiveTime; // memleak
                     std::vector<double> reals;
                     if(pdat.getVertex(i) != ob::PlannerData::NO_VERTEX){
                         if(pdat.getVertex(i).getTag() == 1) {
