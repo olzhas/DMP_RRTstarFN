f9829874bbffa2bf5b371e98f45e923de016be9a
Olzhas Adiyatov <olzhas.adi@gmail.com>
2016-01-22 21:05:16 +0600

    modified:   DRRTstarFN.cpp                 Back to NearestNeighborsLinear 	modified:   weightedrealvectorstatespace.cpp 	modified:   weightedrealvectorstatespace.h                 Playing with coefficients

--- weightedrealvectorstatespace.cpp
+++ weightedrealvectorstatespace.cpp
@@ -22,7 +22,7 @@ namespace base {
                 k = 1.00;
                 break;
             default:
-                k = 1.0;
+                k = 0.0;
                 break;
             }
             dist += (diff * diff) * k;
