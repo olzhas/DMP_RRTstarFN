95fbc8c58cc180e8022a05671dd8abdce87d0260
Olzhas Adiyatov <olzhas.adi@gmail.com>
2016-06-04 12:45:54 +0600

    DynamicSimpleSetup is up-to-date

--- ompl/geometric/src/DynamicSimpleSetup.cpp
+++ ompl/geometric/src/DynamicSimpleSetup.cpp
@@ -1,30 +1,79 @@
-#include "DynamicSimpleSetup.h"
+nclude <chrono>
+#include <thread>
 
-DynamicSimpleSetup::DynamicSimpleSetup(ompl::base::SpaceInformationPtr &si) :
-    ompl::geometric::SimpleSetup(si)
-{
+#include "../DynamicSimpleSetup.h"
 
+namespace ompl {
+namespace geometric {
+
+DynamicSimpleSetup::DynamicSimpleSetup(ompl::base::SpaceInformationPtr &si)
+    : ompl::geometric::SimpleSetup(si) {}
+
+DynamicSimpleSetup::DynamicSimpleSetup(ompl::base::StateSpacePtr &space)
+    : ompl::geometric::SimpleSetup(space) {}
+
+void DynamicSimpleSetup::setup() {
+  SimpleSetup::setup();
+  timestep = std::chrono::milliseconds(100);
 }
 
+void DynamicSimpleSetup::clear() {}
 
-DynamicSimpleSetup::DynamicSimpleSetup(ompl::base::StateSpacePtr &space) :
-    ompl::geometric::SimpleSetup(space)
-{
+void DynamicSimpleSetup::plan() {}
 
+bool DynamicSimpleSetup::drive() {
+  setup();
+  prepare();
+
+  bool terminate = false;
+  while (!terminate) {
+    std::chrono::high_resolution_clock::time_point tNow =
+        std::chrono::high_resolution_clock::now();
+    move();
+    if (!validSolution()) {
+      stop();
+      react();
+    }
+    std::this_thread::sleep_until(tNow + timestep);
+#ifdef DEBUG
+    auto dt = std::chrono::duration_cast<std::chrono::microseconds>(
+                  std::chrono::high_resolution_clock::now() - tNow).count();
+    std::cout << dt << std::endl;
+#endif
+  }
+  return true;
 }
 
+void DynamicSimpleSetup::prepare() {
+  if (!loadPrecomputedData_) {
+    ompl::base::PlannerTerminationCondition ptc(
+        ompl::base::exactSolnPlannerTerminationCondition(
+            getProblemDefinition()));
 
-void DynamicSimpleSetup::setup()
-{
-    timestep = 100;
+    solve(ptc);
+  }
+  // prepareUser();
 }
 
-void DynamicSimpleSetup::clear()
-{
+bool DynamicSimpleSetup::validSolution() {
+  if (planner_ == nullptr) {
+    std::cerr << "planner was not initialized\n";
+    return false;
+  }
 
+  // if (planner_->checkValidity()) return false;
+
+  return true;
 }
 
-void DynamicSimpleSetup::plan()
-{
+void DynamicSimpleSetup::move() {}
 
+void DynamicSimpleSetup::stop() {}
+
+void DynamicSimpleSetup::react() {}
+
+void DynamicSimpleSetup::loadPrecomputed() { /*planner_->load(); */
 }
+}
+}
+
