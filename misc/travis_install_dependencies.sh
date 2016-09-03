c6d0a10aaf17b99f2f713450b2977fbced1c43f9
Olzhas Adiyatov <olzhas.adi@gmail.com>
2016-09-03 18:03:25 +0600

    test

--- misc/travis_install_dependencies.sh
+++ misc/travis_install_dependencies.sh
@@ -21,3 +21,4 @@ sudo apt-add-repository -y ppa:fcl-debs
 sudo apt-add-repository -y ppa:dartsim
 sudo apt-get update -qq
 sudo apt-get install -qq libeigen3-dev libdart5-dev libyaml-cpp-dev
+ls /usr/share/cmake-2.8/
