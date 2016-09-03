b09a4e5f07b9d7da9394260943be2d5fbdd2ca2e
Olzhas Adiyatov <olzhas.adi@gmail.com>
2016-09-03 13:53:04 +0600

    travis CI update

---
+++ misc/travis_before_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+################################################################################
+#
+# Description:
+#	Installs necessary dependencies for Travis-CI
+#
+# Authors:
+#	Olzhas Adiyatov, oadiyatov@nu.edu.kz
+#
+# Year:
+#	2016.
+#
+# Usage:
+#	- This script is automatically called from Travis-CI script
+#
+################################################################################
+
+sudo apt-add-repository -y ppa:libccd-debs
+sudo apt-add-repository -y ppa:fcl-debs
+sudo apt-add-repository -y ppa:dartsim
+sudo apt-get update -qq
+sudo apt-get install -qq libeigen3-dev libdart5-dev
