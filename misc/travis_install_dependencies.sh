caac933baf0f61ef47c08c9309d227fb64030181
Olzhas Adiyatov <olzhas.adi@gmail.com>
2016-09-03 17:11:51 +0600

    test

--- misc/travis_install_dependencies.sh
+++ misc/travis_install_dependencies.sh
@@ -16,7 +16,7 @@
 #
 ################################################################################
 
-sudo apt-get install cmake
+sudo apt-get search cmake
 sudo apt-add-repository -y ppa:libccd-debs
 sudo apt-add-repository -y ppa:fcl-debs
 sudo apt-add-repository -y ppa:dartsim
